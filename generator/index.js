f81498fc2d2648df855c93a4caa6c527f2f505a2
shashikant88093 <shashi94goswami@gmail.com>
2022-07-19 09:49:08 +0530

    generator

---
+++ generator/index.js
@@ -0,0 +1,23 @@
+
+function* numberGen(){
+    let i = 0;
+    while(true){
+        yield i++;
+    }
+}
+const gen = numberGen();
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
