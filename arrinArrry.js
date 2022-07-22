8c625bb9bdf9a5c1a6ab1007bc5f507218930330
shashikant88093 <shashi94goswami@gmail.com>
2022-07-22 12:38:24 +0530

    adedd

--- arrinArrry.js
+++ arrinArrry.js
@@ -1,24 +1,27 @@
 
 const arr = [
-    {name:"a",
-    age:"25"},
     {
-    name:"b",
-    age:"34"
+        name: "a",
+        age: "25"
     },
     {
-    name:"a",
-    age:"45"}
-    ]
-    let newArray = []
-    arr.map((data)=>{
-     if(!newArray[data.name]){
-            newArray[data.name] = []
-           
-        }
-        newArray[data.name].push(data.age)
+        name: "b",
+        age: "34"
+    },
+    {
+        name: "a",
+        age: "45"
     }
+]
+let newArray = []
+arr.map((data) => {
+    if (!newArray[data.name]) {
+        newArray[data.name] = []
+
+    }
+    newArray[data.name].push(data.age)
+}
 
 
-    )
-    console.log(newArray)
+)
+console.log(newArray)
