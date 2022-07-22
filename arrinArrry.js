900238a64b5c950fc05e244995a56c229a051908
Shashikant <42858165+shashikant88093@users.noreply.github.com>
2022-07-22 12:26:53 +0530

    added array in array code

---
+++ arrinArrry.js
@@ -0,0 +1,24 @@
+
+const arr = [
+    {name:"a",
+    age:"25"},
+    {
+    name:"b",
+    age:"34"
+    },
+    {
+    name:"a",
+    age:"45"}
+    ]
+    let newArray = []
+    arr.map((data)=>{
+     if(!newArray[data.name]){
+            newArray[data.name] = []
+           
+        }
+        newArray[data.name].push(data.age)
+    }
+
+
+    )
+    console.log(newArray)
