947403cbc8c7f402f58cb18f4ee547d91809a2b4
shashikant88093 <shashi94goswami@gmail.com>
2022-07-26 23:44:23 +0530

    febinaci with iifc

--- fabinaciSeriel.js
+++ fabinaciSeriel.js
@@ -1,9 +1,20 @@
 //fabinaci series
-function fabinaciSeriel(n) {
-    let fabinaci = [1, 1]
-    for (let i = 2; i < n; i++) {
-        fabinaci.push(fabinaci[i - 1] + fabinaci[i - 2])
+// function fabinaciSeriel(n) {
+//     let fabinaci = [1, 1]
+//     for (let i = 2; i < n; i++) {
+//         fabinaci.push(fabinaci[i - 1] + fabinaci[i - 2])
+//     }
+//     return fabinaci
+// }
+// console.log(fabinaciSeriel(10))
+
+
+(function a(c, d){
+    if(c< 50){
+  
+        console.log(c)
+        a(d, c+d)
     }
-    return fabinaci
-}
-console.log(fabinaciSeriel(10))
+      
+      
+  })(0, 1)
---
+++ ffdd.js
@@ -0,0 +1,10 @@
+
+(function a(c, d){
+    if(c< 50){
+  
+        console.log(c)
+        a(d, c+d)
+    }
+      
+      
+  })(0, 1)
