c53e49ae33a51ad0c196fa903bf2d1ecfd97d49b
shashikant88093 <shashi94goswami@gmail.com>
2023-02-28 11:16:37 +0530

    added

---
+++ practise.js
@@ -0,0 +1,13 @@
+var message = 'hello';
+
+if(true){
+    var message = 'welcome'
+}
+console.log(message)
+
+let message2 = 'hello';
+
+if(true){
+    let message2 = 'welcome'
+}
+console.log(message2)
