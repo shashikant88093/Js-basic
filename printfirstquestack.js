614ee8fa7bd529b38496e9ba76e6b098b79ac5ad
shashikant kumar <shashi94goswami@gmail.com>
2024-07-01 22:04:15 +0530

    added some code snippet of console log stack and que of event loo[

---
+++ printfirstquestack.js
@@ -0,0 +1,16 @@
+console.log("John Doe");
+
+setTimeout(() => {
+    console.log("Jane Doe");
+})
+
+// [ 'John Doe 30 123', 'Joh234'].forEach((element) => console.log(element));
+
+let arr = [ 'John Doe 30 123', 'Joh234']
+
+for(let i=0;i<arr.length;i++){
+
+    console.log(arr[i]);
+
+}
+console.log("Jane Doe 30 123 789");
